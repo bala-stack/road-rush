6f865cfa539cc3186d792bea1880e8cc872cc542
ils-stack <bala@intlogixsol.com>
2020-10-28 13:02:18 +0530

    update

--- js/classes/road.js
+++ js/classes/road.js
@@ -13,14 +13,84 @@ class Road extends Phaser.GameObjects.Container{
 		this.setSize(this.back.displayWidth,game.config.height);
 		
 		this.lineGroup = this.scene.add.group();
+		
+		this.count = 0;
+		this.car = this.scene.add.sprite(this.displayWidth/4,game.config.height*.9,"cars");
+		Align.scaleToGameW(this.car,.10);
+		this.add(this.car);
+		
+		this.back.setInteractive();
+		this.back.on('pointerdown',this.changeLanes,this);
+		
+		 
+		
+		// var keyEvnt = this.Input.Keyboard.Key;
+		
+		// alert(keyEvnt)
+		
+		// for(i in keyEvnt){
+			// alert(i + ' = ' + keyEvnt[i]);
+		// }
+		
+		// var spaceBar = this.back.on('pointerdown',this.changeLanes,this);
+
+		// spaceBar.on('down', listener)
+
+		
+		this.addObject();
 	}
+	
+	addObject(){
+		this.object = this.scene.add.sprite(-this.displayWidth/4,0,"pcar1");
+		Align.scaleToGameW(this.object,.10);
+		this.add(this.object);
+		// this.moveObject();
+	}
+
+	moveObject(){
+		this.object.y += this.vSpace/20;
+		if(this.object.y > game.config.height){
+			this.object.destroy();
+			this.addObject();
+		}	
+	}	
+	
+	changeLanes(){
+		if(this.car.x > 0){
+			this.car.x = -this.displayWidth/4;
+		}else{
+			this.car.x = this.displayWidth/4;	
+		}	
+	}	
+	
+	changeLeft(){
+		this.car.x = -this.displayWidth/4;
+	}
+
+	changeRight(){
+		this.car.x = this.displayWidth/4;	
+	}	
 	
 	makeLines(){
 		this.vSpace = this.displayHeight/10;
-		
-		for(var i = 0;i < 100;i++){
+		for(var i = 0;i < 20;i++){
 			var line = this.scene.add.image(this.x,this.vSpace*i,"line");
+			line.oy = line.y;
 			this.lineGroup.add(line);
 		}	
+	}	
+	
+	moveLines(){
+        this.lineGroup.children.iterate(function(child) {
+            child.y += this.vSpace / 20;
+        }.bind(this));
+        this.count++;
+        if (this.count == 20) {
+            this.count = 0;
+            this.lineGroup.children.iterate(function(child) {
+                child.y = child.oy;
+            }.bind(this));
+        }		
+		
 	}	
 }
--- js/sceneMain.js
+++ js/sceneMain.js
@@ -6,22 +6,40 @@ class SceneMain extends Phaser.Scene {
     	//load images and sounds etc
 		this.load.image("road","images/road.jpg");
 		this.load.image("line","images/line.png");
-		// this.load.spritesheet("cars","images/cars.png",{frameWidth:60,frameHeight:126});
-		this.load.image("cars","images/cars.png",{frameWidth:60,frameHeight:126});
+		this.load.image("pcar1","images/pcar1.png");
+		this.load.image("pcar2","images/pcar2.png");
+		this.load.image("cone","images/cone.png");
+		this.load.image("barrier","images/barrier.png");
+        this.load.spritesheet("cars", "images/cars.png", {
+            frameWidth: 60,
+            frameHeight: 126
+        });;
     }
     create(){
 		//Define our objects
-		var road = new Road({scene:this});
-		road.x = game.config.width/2;
-		road.makeLines();
+		this.road = new Road({scene:this});
+		this.road.x = game.config.width/2;
+		this.road.makeLines();
+		
+		//keyboard capture
+		this.input.keyboard.on('keydown', this.chGameLn, this);
 
         console.log("Ready!");
     }
     update(){
 		//constant running loop
-
+		this.road.moveLines();
+		this.road.moveObject();
 	}
 	
 	//custom functions if required
-	
-}
+	chGameLn(event) {
+		let code = event.keyCode;
+
+		if(code == Phaser.Input.Keyboard.KeyCodes.LEFT){
+			this.road.changeLeft();
+		}else if(code == Phaser.Input.Keyboard.KeyCodes.RIGHT){
+			this.road.changeRight();
+		}	
+	}
+}	
